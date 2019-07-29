53c68795f4bb97034698f5e4ac0739ee183de2fa
David Quiñones <daquinons@users.noreply.github.com>
2019-07-29 10:32:29 +0200

    Fix error in knex config

--- data/migrations/20190729102135_createUsersTable.js
+++ data/migrations/20190729102135_createUsersTable.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', table => {
     table.increments();
-    table.string.unique('username').notNullable();
+    table.string('username').unique().notNullable();
     table.string('password').notNullable();
   });
 };
