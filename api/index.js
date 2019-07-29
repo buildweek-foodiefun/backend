2e864622747e28c5ff2433f7146c50e5a2f006e0
David Quiñones <daquinons@users.noreply.github.com>
2019-07-29 11:05:23 +0200

    Create /auth endpoint for login and register

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const authRouter = require('./auth/router');
 
 const router = express.Router();
 
+
 router.get('/', (req, res) => res.json({ message: 'Welcome to FoodieFun API' }));
+
+router.use('/auth', authRouter);
 
 module.exports = router;
