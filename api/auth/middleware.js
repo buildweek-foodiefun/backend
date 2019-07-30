7afe5052096ef8e14891f02cbaf7477ca149cea8
David Quiñones <daquinons@users.noreply.github.com>
2019-07-30 14:43:24 +0200

    Create middleware to check for the token in the request

--- api/auth/middleware.js
+++ api/auth/middleware.js
@@ -34,11 +34,29 @@ exports.checkBodyForLogin = async (req, res, next) => {
         res.status(401).json({ message: 'Wrong credentials' });
       }
     } else {
-      res.status(400).json({ message: 'Missing required parameter'} );
+      res.status(400).json({ message: 'Missing required parameter' });
     }
   } catch (error) {
     res
       .status(500)
       .json({ message: 'There was an error processing your request' });
+  }
+};
+
+exports.restricted = async (req, res, next) => {
+  try {
+    const token = req.get('Authorization');
+    const payload = helper.validateToken(token);
+    if (token && payload) {
+      // eslint-disable-next-line require-atomic-updates
+      req.user = await Users.findByUsername(payload.username);
+      next();
+    } else {
+      res.status(401).json({
+        message: 'You are not authorized to perform that operation '
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'There was an error in your request' });
   }
 };
